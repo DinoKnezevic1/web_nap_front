8c8b8e471f7932770b5a58b9cb5eb58326dc9416
DinoKnezevic1 <knezevic7p@gmail.com>
2024-06-15 11:23:22 +0200

    first commit

---
+++ sports-calendar/src/Components/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calendar from 'react-calendar';
+import Home from './Home';
+
+function HomePage() {
+  return (
+    <div>
+      <Home/>
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ sports-calendar/src/Components/Location/LocationMain.jsx
@@ -0,0 +1,16 @@
+import LocationDisplay from "./LocationDisplay";
+import LocationPost from "./LocationPost";
+
+
+
+function LocationMain() {
+  return (
+      <div>
+        <h1>Location CRUD for ADMIN</h1>
+         <LocationDisplay />
+         <LocationPost />
+      </div>
+);
+}
+
+export default LocationMain;
